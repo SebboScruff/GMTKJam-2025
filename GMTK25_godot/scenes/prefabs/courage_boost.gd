7762f74c41e6e8f5790e5de9c9b917b18c6adba0
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2025-08-02 11:32:37 +0100

    death and respawn mechanics v1

--- GMTK25_godot/scenes/prefabs/courage_boost.gd
+++ GMTK25_godot/scenes/prefabs/courage_boost.gd
@@ -2,6 +2,7 @@ class_name CourageBoost
 extends Area2D
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
 @export var courage_increase:int
@@ -9,15 +10,13 @@ extends Area2D
 func on_pickup(_player:Player) -> void:
 	print("%s picked up by Player"%name)
 	# Disable the Area Monitor Status
-	monitorable = false
+	collision_shape_2d.disabled = true
 	# Hide the sprite
 	sprite.set_visible(false)
 	_player.add_wisps(courage_increase)
-	queue_free()
 	
 func reset() -> void:
-	pass
-	# re-enable the Area Monitor Status
-	monitorable = true
+	# re-enable the collider
+	collision_shape_2d.disabled = false
 	# Show the sprite
 	sprite.set_visible(true)
