3efbcf5a46f694bc840c528c56b5095bf01a53c4
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2025-08-03 14:31:13 +0100

    added fog back in

--- GMTK25_godot/scenes/menus/main_menu.gd
+++ GMTK25_godot/scenes/menus/main_menu.gd
@@ -10,13 +10,23 @@ func _ready() -> void:
 	## Connect up button signals here
 	pass
 
+func _physics_process(delta: float) -> void:
+	if(Input.is_action_just_pressed("menu_go_back")):
+		show_home_page()
+
 func load_game() -> void:
 	get_tree().change_scene_to_packed(LEVEL)
 	
 func show_home_page() -> void:
 	home_page.set_visible(true)
+	home_page.set_process(true)
+	
 	instruction_screen.set_visible(false)
+	instruction_screen.set_process(false)
 	
 func show_instructions() -> void:
 	home_page.set_visible(false)
+	home_page.set_process(false)
+	
 	instruction_screen.set_visible(true)
+	instruction_screen.set_process(true)
